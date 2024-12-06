0e80ea1fd40922795e40c44ba103c80afc8f34b4
Stmg2025 <cwts007@gmail.com>
2024-12-05 21:24:16 -0300

    cambios se agrega ruta

--- routes/cart.js
+++ routes/cart.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const { addToCart, getCart, removeFromCart } = require('../controllers/cartController');
+const { addToCart, getCart, updateCart, removeFromCart } = require('../controllers/cartController');
 const authenticate = require('../middleware/authenticate'); // Middleware de autenticación
 
 const router = express.Router();
 
 // Rutas del carrito protegidas por el middleware
-router.post('/', authenticate, addToCart);
-router.get('/', authenticate, getCart);
-router.delete('/:id', authenticate, removeFromCart);
+router.post('/', authenticate, addToCart); // Agregar al carrito
+router.get('/', authenticate, getCart); // Obtener el carrito
+router.put('/:id', authenticate, updateCart); // Actualizar cantidad en el carrito
+router.delete('/:id', authenticate, removeFromCart); // Eliminar del carrito
 
 module.exports = router;
